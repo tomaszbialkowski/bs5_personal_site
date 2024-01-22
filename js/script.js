c94cab92e33b9826080fa403ec344c25552dc5c4
tomaszbialkowski <tomaszbialkowski78@gmail.com>
2024-01-22 22:42:39 +0100

    done with header and typing effect

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
 // Add custom JavaScript here
+function runTypingEffect() {
+  const text = "WhiteCowSky";
+  const typingElement = document.getElementById("typing-text");
+  const typingDelay = 130;
+
+  typetext(text, typingElement, typingDelay);
+}
+
+function typetext(text, typingElement, delay) {
+  for (let i = 0; i < text.length; i++) {
+    setTimeout(() => {
+      typingElement.textContent += text.charAt(i);
+    }, delay * i);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", runTypingEffect);
